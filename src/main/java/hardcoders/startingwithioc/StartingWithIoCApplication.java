c2d28be06d6f9af16624a1d72c86205fce2af59b
Ali Shahbazi <alishahbazi799@gmail.com>
2023-05-09 11:27:17 -0400

    StartingWithIoC

---
+++ src/main/java/hardcoders/startingwithioc/StartingWithIoCApplication.java
@@ -0,0 +1,14 @@
+package hardcoders.startingwithioc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartingWithIoCApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(StartingWithIoCApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hardcoders/startingwithioc/StartingWithIoCApplicationTests.java
@@ -0,0 +1,13 @@
+package hardcoders.startingwithioc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartingWithIoCApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
