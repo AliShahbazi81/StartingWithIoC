f4d343aa9ffef6db2fda94f7290e985eeb81d021
Ali Shahbazi <alishahbazi799@gmail.com>
2023-05-10 19:19:51 -0400

    SettingUpTheMySqlWithTheProject

--- src/main/java/hardcoders/startingwithioc/StartingWithIoCApplication.java
+++ src/main/java/hardcoders/startingwithioc/StartingWithIoCApplication.java
@@ -1,7 +1,9 @@
 package hardcoders.startingwithioc;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StartingWithIoCApplication {
@@ -9,6 +11,16 @@ public class StartingWithIoCApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(StartingWithIoCApplication.class, args);
+    }
+
+    // From the Spring Boot Framework
+    // Executed after the Spring Beans have been loaded
+    @Bean
+    public CommandLineRunner commandLineRunner(String[] args)
+    {
+        return runner -> {
+            System.out.println("Hello World!");
+        };
     }
 
 }
